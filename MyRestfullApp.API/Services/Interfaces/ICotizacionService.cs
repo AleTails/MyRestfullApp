267fd78e7f499b86b2bc29f86188bf680ab8cd2d
AleTails <AleTails@gmail.com>
2018-05-04 02:06:22 -0300

    Create CotizacionController and its services

---
+++ MyRestfullApp.API/Services/Interfaces/ICotizacionService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyRestfullApp.API.Services.Interfaces
+{
+    public interface ICotizacionService
+    {
+        Task<IActionResult> Cotizar();
+    }
+}
---
+++ MyRestufllApp.API.Tests/Services/CotizacionDolarTests.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyRestfullApp.API.Services;
+using Xunit;
+
+namespace MyRestufllApp.API.Tests.Services
+{
+    public class CotizacionDolarTests
+    {
+        [Fact]
+        public async Task CotizacionDolar_ShouldRetunr_Ok()
+        {
+            // Arrange
+            var cotizacionDolarService = new CotizacionDolarService();
+
+            // Act
+            var result = await cotizacionDolarService.Cotizar();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result);
+        }
+    }
+}
